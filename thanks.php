27d4045d8a2b0ab8611ef48e3426da516f92251d
Wacil23 <wacilzek00@gmail.com>
2022-02-11 17:21:20 +0100

    petite modif

--- thanks.php
+++ thanks.php
@@ -1,3 +1,3 @@
 <?php 
-echo ('Merci ' . $name . ' de nous avoir contacté à propos de ' . $_POST['subject']);
+echo ('Merci ' . $_POST['user_name'] . ' de nous avoir contacté à propos de ' . $_POST['subject']);
 echo (' Un de nos conseiller vous contactera soit à l’adresse ' . $_POST['user_email'] . 'ou par téléphone au ' . $_POST['phone'] . ' dans les plus brefs délais pour traiter votre demande : ' . $_POST['user_message'] );
